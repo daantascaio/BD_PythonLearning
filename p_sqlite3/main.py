75174f6800cbe593ebe29cf02154920e1f92a634
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-10 16:35:11 -0300

    create: table

--- p_sqlite3/main.py
+++ p_sqlite3/main.py
@@ -6,10 +6,18 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 # Open connection with my DB
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, weight REAL)'
+)
+
+connection.commit()
 
 # Close my connection with DB
 cursor.close()
